e1386a4f87bcf4ae10ae4744354de978547b421a
connorjun12345 <cjun1@hwemail.com>
2025-08-28 14:54:42 -0700

    Worked August 28th
    .

---
+++ Day 0/Lamp.java
@@ -0,0 +1,62 @@
+public class Lamp {
+    // 1 light bulb
+    // on/off switch
+    // electrical cord
+    // white lamp shade
+    // silver pole
+
+    private int numOfBulbs; // = 1;
+    private String body; // = "Silver";
+    private String part; // = "cord";
+    private String on_or_off; // = "power switch";
+    private String color; // = "White lamp shade";
+    private double height_feet; // = 1.5;
+    private int lampBrightness; 
+
+    public Lamp() {
+        numOfBulbs = 1;
+        body = "silver";
+        part = "chord";
+        on_or_off = "power switch";
+        color = "White lamp shade";
+        height_feet = 1.5;
+        lampBrightness = 10;
+    }
+
+
+    public void dim() {
+        lampBrightness = 20;
+        System.out.println("The lamp is now on brightness level " + lampBrightness);
+    }    
+
+    public void shine() {
+        color = "yellow";
+        System.out.println("The lamp is now shining " + color);
+    }    
+
+    public void shatter() {
+        System.out.println("The lamp fell and shattered!!!");
+    }
+
+    public void on() {
+        on_or_off = "on";
+        System.out.println("The lamp is now turned");
+        System.out.println(on_or_off);
+    }
+
+    public void off() {
+        on_or_off = "off";
+        System.out.println("The lamp is now turned");
+        System.out.println(on_or_off);
+    }
+    
+    //illuminate
+    // shine
+    // break
+    // fix
+    //on
+    //off
+    public static void main() {
+        
+    }
+}
---
+++ Day 0/LampTester.java
@@ -0,0 +1,12 @@
+public class LampTester {
+    
+    public static void main(String[] args) {
+        System.out.println("Yo, whats up");
+
+        Lamp colorfulLamp = new Lamp();
+        colorfulLamp.shine();
+        colorfulLamp.on();
+        colorfulLamp.off();
+        colorfulLamp.shatter();
+    }
+}
